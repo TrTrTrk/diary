efa313f3fb73c65bb5e01a85bf66a45149033e8e
Trk <h.trk.85@gmail.com>
2023-04-16 14:06:25 +0900

    generation limit. Random display of images on the main screen. Loading screen fix

--- app/Http/Controllers/InputController.php
+++ app/Http/Controllers/InputController.php
@@ -5,18 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use PhpParser\Builder\Function_;
+use Illuminate\Support\Facades\Auth;
+use DB;
 
 class InputController extends Controller
 {
     private $sentencesCount = 5;
-   
+
     public function input()
-    {
-        // inputLineCountはユーザー設定の入力行数
-        // 20230338 今は5個に固定   
+    { 
 
-        return view('input',['Counts' => range(0, $this->sentencesCount - 1) ]);
+        $id = Auth::id();
 
+        $count = DB::table('users')->Where('id', $id)->get(['create_count']);
+
+        return view('input', ['line_counts' => range(0, $this->sentencesCount - 1), 'create_count' => $count[0]->create_count]);
     }
-
 }
