65079814a62effa74b8206404189d940b410083b
Trk <h.trk.85@gmail.com>
2023-04-24 14:29:03 +0900

    Main (list) screen display adjustment

--- resources/js/components/ListComponent.vue
+++ resources/js/components/ListComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center" v-if="isLogin">
+        <div class="row justify-content-center my-2" v-if="canMakePic">
             <div class="col-md-8"></div>
             <div class="col-md-4 text-center">
                 <a class="btn btn-primary" href="/input">make diary</a>
@@ -12,7 +12,7 @@
             <div class="col-md-4 text-center my-2" v-for="(picItem, index) in picItems" :key="index">
                 <div class="card">
                     <a :href='"/disp?imageName=" + picItem.imageName'>
-                        <img class="border border-dark" :src='"storage/" + picItem.id + "/" + picItem.imageName'
+                        <img class="border border-dark" :src='"./storage/" + picItem.id + "/" + picItem.imageName'
                             :alt="picItem.sentences" />
                     </a>
                 </div>
@@ -25,7 +25,7 @@
 export default {
     props: {
         picItems: Array,
-        isLogin: Boolean
+        canMakePic: Boolean
     },
     mounted() {
         console.log('Component mounted.')
